cdbc19f1358118ab74f8e5d16eead34214fc5171
Ben Cooper <benc@hey.com>
2024-10-25 17:27:38 -0700

    try building a lib

--- main.cc
+++ main.cc
@@ -1,43 +1,59 @@
-
-#include <iostream>
-#include <chrono>
+#include <emscripten.h>
+#include <emscripten/bind.h>
 
 #include "s2/s2cell_id.h"
 #include "s2/s2cell.h"
 #include "s2/s2latlng.h"
 #include "s2/s2latlng_rect.h"
 
-int main()
+using namespace emscripten;
+
+struct LatLng
 {
-    auto start = std::chrono::high_resolution_clock::now();
-    // Create a cell from a token
-    S2CellId cell_id = S2CellId::FromToken("89c25");
-    auto end = std::chrono::high_resolution_clock::now();
-    auto duration = std::chrono::duration_cast<std::chrono::microseconds>(end - start);
-    std::cout << "Time taken to create cell: " << duration.count() << " microseconds\n";
+    double lat;
+    double lng;
+};
+struct S2CellInfo
+{
+    std::string id;
+    LatLng low;
+    LatLng high;
+    double approximate_area;
+};
 
-    start = std::chrono::high_resolution_clock::now();
-    // Get the cell
+S2CellInfo GetCellInfo(const std::string token)
+{
+    S2CellId cell_id = S2CellId::FromToken(token);
     S2Cell cell(cell_id);
-    end = std::chrono::high_resolution_clock::now();
-    duration = std::chrono::duration_cast<std::chrono::microseconds>(end - start);
-    std::cout << "Time taken to get cell: " << duration.count() << " microseconds\n";
-
-    start = std::chrono::high_resolution_clock::now();
-    // Get the cell's bounding rectangle
     S2LatLngRect bounds = cell.GetRectBound();
-    end = std::chrono::high_resolution_clock::now();
-    duration = std::chrono::duration_cast<std::chrono::microseconds>(end - start);
-    std::cout << "Time taken to get cell's bounding rectangle: " << duration.count() << " microseconds\n";
 
-    start = std::chrono::high_resolution_clock::now();
-    // Print the cell's extents
-    std::cout << "Cell extents:\n";
-    std::cout << "Southwest: " << bounds.lo() << "\n";
-    std::cout << "Northeast: " << bounds.hi() << "\n";
-    end = std::chrono::high_resolution_clock::now();
-    duration = std::chrono::duration_cast<std::chrono::microseconds>(end - start);
-    std::cout << "Time taken to print cell's extents: " << duration.count() << " microseconds\n";
+    S2CellInfo cell_info;
+    cell_info.id = token;
 
-    return 0;
+    auto lo_bounds = bounds.lo();
+    auto hi_bounds = bounds.hi();
+
+    cell_info.low.lat = lo_bounds.lat().degrees();
+    cell_info.low.lng = lo_bounds.lng().degrees();
+    cell_info.high.lat = hi_bounds.lat().degrees();
+    cell_info.high.lng = hi_bounds.lng().degrees();
+
+    cell_info.approximate_area = cell.ApproxArea();
+
+    return cell_info;
+}
+
+EMSCRIPTEN_BINDINGS(get_cell_info)
+{
+    class_<LatLng>("LatLng")
+        .property("lat", &LatLng::lat)
+        .property("lng", &LatLng::lng);
+
+    class_<S2CellInfo>("S2CellInfo")
+        .property("id", &S2CellInfo::id)
+        .property("low", &S2CellInfo::low)
+        .property("high", &S2CellInfo::high)
+        .property("approximate_area", &S2CellInfo::approximate_area);
+
+    function("GetCellInfo", &GetCellInfo);
 }
