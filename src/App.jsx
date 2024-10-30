4b30abc5e01715f283c9cfafce050b15ea4c1d57
Ben Cooper <benc@hey.com>
2024-10-29 19:41:21 -0700

    Reverse geocode

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,11 @@
 import s2WasmModulePromise from './wasm/s2_library';
-import { createSignal, Switch, Match, createEffect } from 'solid-js';
+import {
+  createSignal,
+  Switch,
+  Match,
+  createEffect,
+  createResource,
+} from 'solid-js';
 
 const numFormatter = new Intl.NumberFormat();
 const steradiansToDisplayArea = (steradians) => {
@@ -27,20 +33,51 @@ function Field(props) {
   );
 }
 
+const GeocodeComponent = (props) => {
+  console.log(props);
+  const [locationStr] = createResource(async () => {
+    const url = `https://nominatim.openstreetmap.org/reverse?lat=${props.lat}&lon=${props.lng}&format=json`;
+    const response = await fetch(url);
+
+    if (!response.ok) {
+      return null;
+    }
+    const data = await response.json();
+
+    if (data.error) return 'Unknown';
+
+    let locationStr = '';
+    if (data.address) {
+      locationStr = `${data.address.city}, ${data.address.state}`;
+
+      // The next block of code should be exclusively read whilst listening to the national anthem.
+      // Loudly reciting the Pledge of Allegiance will do in a pinch.
+      if (data.address.country_code !== 'us') {
+        locationStr += `, ${data.address.country}`;
+      }
+    }
+    console.log(locationStr);
+    return locationStr;
+  });
+
+  return (
+    <Suspense fallback={<Field label={props.label} value="Loading..." />}>
+      <Field label={props.label} value={locationStr()} />
+    </Suspense>
+  );
+};
+
 function CellInfo(props) {
-  console.log('hi', props);
   return (
     <div id="cell-info">
       <Switch>
         <Match
-          when={props.cellInfoOutput != undefined && props.cellInfoOutput.error}
+          when={props.cellInfoOutput != undefined && !props.cellInfoOutput.ok}
         >
           <div class="label">❌ Invalid Cell ID</div>
         </Match>
         <Match
-          when={
-            props.cellInfoOutput != undefined && !props.cellInfoOutput.error
-          }
+          when={props.cellInfoOutput != undefined && props.cellInfoOutput.ok}
         >
           <button
             on:click={navigator.clipboard.writeText(
@@ -76,6 +113,19 @@ function CellInfo(props) {
               props.cellInfoOutput.value.approximateArea,
             )}
           />
+          <GeocodeComponent
+            label="Centroid Reverse Geocode"
+            lat={
+              (props.cellInfoOutput.value.low.lat +
+                props.cellInfoOutput.value.high.lat) /
+              2
+            }
+            lng={
+              (props.cellInfoOutput.value.low.lng +
+                props.cellInfoOutput.value.high.lng) /
+              2
+            }
+          />
         </Match>
       </Switch>
     </div>
@@ -85,10 +135,10 @@ function CellInfo(props) {
 const calculateCellInfo = (s2Module, cellId) => {
   if (!s2Module || !cellId) return null;
   try {
-    return { error: false, value: s2Module.GetCellInfo(cellId) };
+    return { ok: true, value: s2Module.GetCellInfo(cellId) };
   } catch (error) {
     console.error('Error processing cell ID:', error);
-    return { error: true };
+    return { ok: false };
   }
 };
 
@@ -96,6 +146,7 @@ function App() {
   const [s2WasmModule, sets2WasmModule] = createSignal(null);
   const [inputCellId, setInputCellId] = createSignal('');
   const [cellInfoOutput, setCellInfoOutput] = createSignal(null);
+
   s2WasmModulePromise().then((s2Func) => {
     sets2WasmModule(s2Func);
   });
